82079e0fdd5d2723fad196ba9a8a27484c557b29
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-16 13:04:35 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * int mane - entry point
+ * input (Programming is like building a multilingual puzzle)
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return(0);}
