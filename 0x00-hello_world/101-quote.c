06e05894354e5085b5b68709007ff71db3e43e09
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-16 18:16:57 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 	FILE *fp;
-	char str[] = "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\"
+		\ - Dora Korpar, 2015-10-19\n";
 
 	fp = fopen("quote.c", "w");
 	fwrite(str, 1, sizeof(str), fp);
