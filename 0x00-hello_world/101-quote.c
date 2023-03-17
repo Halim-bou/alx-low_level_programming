adeeed282c57e7ad827cc44d4c86359fc56cd531
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-16 17:39:35 -0700

    qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include <iostream>
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 ;
 }
