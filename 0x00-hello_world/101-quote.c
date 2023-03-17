ec8e2dcba6597929ebfc49a92151115ef2a33bef
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-16 18:59:11 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-#include <sys/syscall.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - print the text followed with anew line
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\""
-		" - Dora Korpar, 2015-10-19\n";
-
-	syscall(SYS_write, 1, msg, sizeof(msg));
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+	;
 }
