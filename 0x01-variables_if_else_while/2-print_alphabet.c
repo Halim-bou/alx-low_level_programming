8174f297b0dda3d9d521fa66acc49dc7a88a892c
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-17 16:09:17 -0700

    print alphabelt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - alphabet using putchar
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+
+	char ch = start;
+
+	while (ch <= end)
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
