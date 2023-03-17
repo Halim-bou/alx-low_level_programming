730351c42d29737608ace279cf3dee0109d5c058
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-16 18:33:35 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/syscall.h>
 
 /**
  * main - Entry point
@@ -6,6 +7,10 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	const char msg[] = "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n";
+
+	syscall(SYS_write, 1, msg, sizeof(msg));
+
 	return (1);
 }
