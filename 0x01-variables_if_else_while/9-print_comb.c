981f799b9b3574081cdb2b82dc08cf2e4fb9c0ef
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-17 19:57:58 -0700

    commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - all possible combinations of single-digit numbers.
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
