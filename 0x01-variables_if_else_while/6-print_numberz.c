57c768245ea99ebff8f83b3ef470aa26ba90ed88
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-17 19:19:02 -0700

    print numbers without printf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the numbers from 0 to 10
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
