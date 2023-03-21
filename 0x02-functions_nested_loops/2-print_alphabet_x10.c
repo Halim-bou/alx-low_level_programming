16459b7e7dc9c8ba2ed0ee5e990efca06ca9ad0d
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-21 14:49:06 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 time.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char a ;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+
+		i++;
+	}
+}
