20c379ed4128bb3b17d98e94d26e9874d6060920
abdelhalimelbouaami <hbouaami@gmail.com>
2023-03-24 15:06:08 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,11 +17,11 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size; j--)
+			for (j = size - i; j > 1; j--)
 			{
 				_putchar(32);
 			}
-			for (k = 0; k < size; k++)
+			for (k = 0; k <= i; k++)
 			{
 				_putchar(35);
 			}
