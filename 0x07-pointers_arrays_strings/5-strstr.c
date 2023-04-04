7b160d6065700933f36e12cf8ef85bfbbab07be4
abdelhalimelbouaami <hbouaami@gmail.com>
2023-04-03 19:02:42 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (; needle[j] != '\0'; j++)
 		{
-			if (haystack[i +j] != needle[j])
+			if (haystack[i + j] != needle[j])
 			{
 				break;
 			}
