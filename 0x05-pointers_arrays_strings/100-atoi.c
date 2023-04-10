cf400186fc84c91ee7dc785c644013de6b45d4ca
abdelhalimelbouaami <hbouaami@gmail.com>
2023-04-10 14:56:30 -0700

    atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - function atoi
+ * @s: string to convert
+ *
+ * Return: Always (success)
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int k = 1;
+	int n = 0;
+
+	while (s[i])
+	{
+	if (s[0] == '-')
+	{
+		k = -1;
+		i++;
+	}
+	for (; s[i] >= '0' && s[i] <= '9'; i++)
+	{
+		j = j * 10 + s[i] - '0';
+		n = 1;
+	}
+	if (n == 1)
+	{
+		break;
+	}
+	i++;
+	}
+	return (k * j);
+}
