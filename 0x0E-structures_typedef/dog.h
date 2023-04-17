2833e3ab02d3b2076bdf94da8e4fcb4a5cf2701e
abdelhalimelbouaami <hbouaami@gmail.com>
2023-04-17 15:50:05 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - define new type
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: The Dog owner.
+ */
+
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
 #endif
