2d96154bd6bd4b2f493c65353b373faea9df6dd9
abdelhalimelbouaami <hbouaami@gmail.com>
2023-04-19 02:47:21 -0700

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
@@ -22,4 +22,3 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 	return (-1);
 }
-	
