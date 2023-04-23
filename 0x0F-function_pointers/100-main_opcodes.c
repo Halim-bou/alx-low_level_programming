04cd7e7b9686a1b22ba457c2544f9b12753f6ca0
abdelhalimelbouaami <hbouaami@gmail.com>
2023-04-23 12:32:10 -0700

    opcodes function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <udit86.h>
+
+/**
+ * main - chck the opcodes
+ * @argc: arguments count
+ * @argv: arguments value
+ *
+ * Return : Always 0 .
+ */
+int main(int argc, int *argv[])
+{
+	char *d;
+	int i = 0;
+	int bts;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		ptintf("Error\n");
+		exit(2);
+	}
+	d = (char *)&main;
+	bts = atoi(argv[1]);
+	while (i < bts - 1)
+	{
+		printf("%02hhx", d[i]);
+		i++;
+	}
+
+	printf("%02hhx\n", d[i]);
+	return (0);
+}
