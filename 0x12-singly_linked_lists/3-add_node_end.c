031589df4ad41c39eaf9790bae9a53bfa58aa7be
abdelhalimelbouaami <hbouaami@gmail.com>
2023-04-27 15:18:35 -0700

    add a node in the end of list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,7 +9,7 @@
  *
  * Return: the address of the element, or NULL if it failed.
  */
-list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *temp;
 	list_t *nlist;
