e21b4ef85b62bce162e74ee1dc119f17dcc951b7
abdelhalimelbouaami <hbouaami@gmail.com>
2023-05-01 10:44:25 -0700

    free all node in the linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - free all nodes in a linked list
+ * @head: the pointer of the first node
+ *
+ * Return: Nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
