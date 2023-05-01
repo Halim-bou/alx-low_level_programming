e45267df922c80b305e25989ea7f733470b9d2e8
abdelhalimelbouaami <hbouaami@gmail.com>
2023-05-01 12:57:20 -0700

    pop listint

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - 
+ * @head:
+ *
+ * Retrun:
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int data;
+
+	ptr = *head;
+	if (ptr != NULL)
+	{
+		ptr = ptr->next;
+		data = ptr->n;
+		free(ptr);
+		*head = ptr;
+	}
+	return (data);
+}
