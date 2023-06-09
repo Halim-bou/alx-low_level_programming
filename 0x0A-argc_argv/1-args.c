5ae5a9e8b15392fd8661a2b05937b143e5eec48c
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-06-09 14:40:01 -0700

    number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: argument counter
+ * @argv: argument value
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
