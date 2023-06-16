af41f762453691d42e673714313e3b5ca9d9b062
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-06-16 09:11:43 -0700

    array range task

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,10 +16,12 @@ int *array_range(int min, int max)
 		return (NULL);
 	size = max - min;
 	array = malloc(sizeof(int) * (size + 1));
+	i = 0;
 	j = min;
-	for (i = 0; j <= max; i++)
+	while (j <= max)
 	{
 		array[i] = j;
+		i++;
 		j++;
 	}
 	return (array);
