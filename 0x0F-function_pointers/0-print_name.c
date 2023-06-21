304c5a1cfd2b56f6512023aeaa1ca7fff970bbee
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-06-21 12:36:21 -0700

    function that print the depanding on the function given

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stddef.h>
 /**
  * print_name - function that print the name given
  * @name: The anme of the person
@@ -9,5 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+		return;
 	f(name);
 }
