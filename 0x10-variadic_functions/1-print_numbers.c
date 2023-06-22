9d1b8907840e407e53142364aa630984c15f62f8
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-06-22 11:26:58 -0700

    creat a function that print just numbers with separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,10 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 
 	va_start(numbers, n);
+
 	for (; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
-		if ( i < n - 1 && separator != NULL)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
