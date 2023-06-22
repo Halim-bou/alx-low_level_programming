5103848e86508adb4c5c91e74badfd257c6091bb
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-06-22 12:19:25 -0700

    function that print the string elements wit separator

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,19 +12,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *c;
 	unsigned int i = 0;
 
+
 	if (n > 0)
 	{
-		va_start(str, n);
-		while (i < n)
-		{
-			c = va_arg(str, char *);
-			if (str != NULL)
-				printf("%s", c);
-			if (str == NULL)
-				printf("(nil)");
-			if (i < n - 1 && separator != NULL)
-				printf("%s", separator);
-			i++;
+	va_start(str, n);
+	while (i < n)
+	{
+		c = va_arg(str, char *);
+		if (str != NULL)
+			printf("%s", c);
+		if (str == NULL)
+			printf("(nil)");
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+		i++;
+	}
 	}
 	va_end(str);
 	printf("\n");
