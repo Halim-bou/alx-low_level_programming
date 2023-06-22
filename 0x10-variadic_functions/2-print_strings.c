50dca658e5c93ed415f4e06b8b50c940b551e8b2
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-06-22 12:43:15 -0700

    last task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		c = va_arg(str, char *);
 		if (str == NULL)
 			printf("(nil)");
-		if (str != NULL)
+		else
 			printf("%s", c);
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
