3b1e25e575d13d2522b9e50faa1252e45120cf7b
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-07-04 12:43:28 -0700

    task 2 create function thatadd a new node3 at the biginning of linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
 /**
  * add_nodeint - add a new node at the beginning of linked list
  * @head: a pointer to the the pointer of the first node
- * @h: a pointer to the first node
+ * @n: a pointer to the first node
  *
  * Return: the address of the new linked list
  */
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (n_node == NULL)
 		return (NULL);
 	n_node->n = n;
-	n_node->next= *head;
+	n_node->next = *head;
 	*head = n_node;
 	return (n_node);
 }
