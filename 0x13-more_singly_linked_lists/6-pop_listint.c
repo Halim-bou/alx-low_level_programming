9df43a727e094ba9d0b04ee7972963320febbd6b
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-07-05 04:02:21 -0700

    function that delete the node of a linked list and return the value stored

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - function to delete the hed of linked list
+ * @head: a pointer to the pointer to the first node
+ * Return: the value of the node deleted
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int val;
+
+	node = *head;
+	if (node == NULL)
+		return (NULL);
+	val = node->n;
+	*head = node->next;
+	free(node);
+	return (val);
+}
