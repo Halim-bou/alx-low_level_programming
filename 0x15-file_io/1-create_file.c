8dbeadd30eba4e6b06277f59ec2c0100c3d6b25d
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-07-10 20:02:03 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,10 +9,9 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	int file, len, closed;
+	int file, len = 0, closed;
 	mode_t mode = S_IRUSR | S_IWUSR;
 
-	len = 0;
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,11 +6,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#define BUFSIZE 1024
 
 /**functions for task 0 -> 4*/
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
-
+int create_file(const char *filename, char *text_content);
 
 
 #endif
