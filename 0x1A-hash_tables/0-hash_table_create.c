5b74929b8e68583e20252bd80901d2601e8b5654
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-07 15:01:13 -0700

    check the size first

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 
+	if (size < 1)
+		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
