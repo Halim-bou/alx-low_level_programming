5333ed302d6ebf72d2ffb07e3243a830fcca6f95
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-07 15:05:42 -0700

    ???

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
+	unsigned long int i = 0;
 
 	if (size < 1)
 		return (NULL);
 	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (table == NULL)
 		return (NULL);
+	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!table->array)
+	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
-	table->size = size;
+	for (; i < size; i++)
+		table->array[i] = NULL;
+
 	return (table);
 }
