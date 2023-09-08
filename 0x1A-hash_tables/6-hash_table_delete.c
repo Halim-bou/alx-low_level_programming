33eccf2998e6f21ff07cc2b44055ecf623709985
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-08 14:38:25 -0700

    fix betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,12 +6,12 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *ptr, *temp;
+	hash_node_t *ptr = NULL, *temp = NULL;
 	unsigned long int i = 0;
 
 	if (ht->array)
 	{
-		for (;i < ht->size; i++)
+		for (; i < ht->size; i++)
 		{
 			if (ht->array[i])
 			{
