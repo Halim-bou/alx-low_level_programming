35b4aadfb4a5786b5b361f80a5ff56cf0ac4ce3c
aelbouaa <hbouaami@gmail.com>
2024-06-30 15:51:15 +0100

    some corrections

--- ruch42/ex00/main.c
+++ ruch42/ex00/main.c
@@ -1,38 +1,19 @@
- #include <unistd.h>
  void ft_putchar(char c);
  void   rush(int x, int y); 
 
 
 int main(void)
 {
-    // write(1, "####################", 20);
-    ft_putchar('\n');
+    
     rush(1, 1);
-    // write(1, "####################", 20);
-    ft_putchar('\n');
     rush(5, 5);
-    // write(1, "####################", 20);
-    ft_putchar('\n');
     rush(5, 1);
-    // write(1, "####################", 20);
-    ft_putchar('\n');
     rush(1, 5);
-    // write(1, "####################", 20);
-    ft_putchar('\n');
-    rush(0, 1);
-    // write(1, "####################", 20);
-    ft_putchar('\n');
+    rush(0, 0);
     rush(1, 0);
-    // write(1, "####################", 20);
-    ft_putchar('\n');
+    rush(0, 1);
     rush(-5, 5);
-    // write(1, "####################", 20);
-    ft_putchar('\n');
     rush(5, -5);
-    // write(1, "####################", 20);
-    ft_putchar('\n');
     rush(-5, -5);
-    // write(1, "####################", 20);
-    ft_putchar('\n');
     rush(2147483648, -2147483648);
 }
